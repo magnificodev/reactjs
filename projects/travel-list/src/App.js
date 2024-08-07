3fdae8d276e61fa5de7c4f04a466fb2c9664aef9
magnificodev <magnificodev8@gmail.com>
2024-08-07 21:46:48 +0700

    Lifting state

--- projects/travel-list/src/App.js
+++ projects/travel-list/src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+    const [items, setItems] = useState([]);
+
+    const handleAddItems = (newItem) => {
+        setItems((prevItems) => [...prevItems, newItem]);
+    };
+
     return (
         <div className="app">
             <Logo />
-            <Form />
-            <PackingList />
+            <Form onAddItems={handleAddItems} />
+            <PackingList items={items} />
             <Stats />
         </div>
     );
@@ -21,7 +27,7 @@ function Logo() {
     return <h1>🏝️ Far away 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
     const [description, setDescription] = useState("");
     const [quantity, setQuantity] = useState(1);
 
@@ -37,8 +43,11 @@ function Form() {
             id: Date.now(),
         };
         console.log(newItem);
-        setDescription("")
-        setQuantity(1)
+
+        onAddItems(newItem);
+
+        setDescription("");
+        setQuantity(1);
     };
 
     return (
@@ -66,11 +75,11 @@ function Form() {
     );
 }
 
-function PackingList() {
+function PackingList({ items }) {
     return (
         <div className="list">
             <ul>
-                {initialItems.map((item) => (
+                {items.map((item) => (
                     <Item item={item} key={item.id} />
                 ))}
             </ul>
